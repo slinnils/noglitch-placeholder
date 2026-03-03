cec0e62c97c5ca7777729e1a8dcd993fa6e4d6dd
slinnils <imbachnils@hotmail.ch>
2026-03-04 00:31:21 +0100

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="text-slate-400 flex flex-col items-center justify-center h-screen">
-      <h1 className="text-stone-400 text-4xl font-semibold">
+    <div className="text-slate-400 flex flex-col items-center justify-center h-screen mx-8">
+      <h1 className="text-stone-400 text-[clamp(2rem,5vw,2.5rem)] font-semibold">
         Webentwicklung und UI/UX Design aus Graubünden
       </h1>
       <div className="flex flex-col mt-10">
-        <p className="text-2xl">Meine Website ist gerade im Aufbau.</p>
-        <p className="text-xl">Bei Fragen oder Anfragen erreichst du mich hier:</p>
-        <p className="mt-4">
+        <p className="text-[clamp(1.3rem,5vw,1.8rem)]">Meine Website ist gerade im Aufbau.</p>
+        <p className="text-[clamp(1rem,5vw,1.5rem)]">Bei Fragen oder Anfragen erreichst du mich hier:</p>
+        <p className="mt-8">
             <a href="mailto:noglit@proton.me" className="btn text-stone-300 hover:text-stone-800 bg-slate-600 hover:bg-slate-400 py-2 px-4 rounded-sm">
               Kontakt aufnehmen
             </a>
